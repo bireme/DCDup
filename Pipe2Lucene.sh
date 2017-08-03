953e8a3927712cec6bead9b002544d5f0b7ecf79
Heitor <heitor@bir927595.wdc.paho.org>
2017-08-03 11:16:01 -0300

    hidding shells

--- Pipe2Lucene.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/javaapps/sbt-projects/DCDup
-
-sbt "run-main org.bireme.dcdup.Pipe2Lucene $1 $2 $3 $4 $5 $6"
-
-cd -
--- exemplo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DCDUP_HOME=/home/heitor/sbt-projects/DCDup
-#DCDUP_HOME=/home/javaapps/sbt-projects/DCDup
-
-java -cp $DCDUP_HOME/target/scala-2.12/DCDup-assembly-0.1.0.jar org.bireme.dcdup.WebDoubleCheckDuplicated LILACSLike_Sas.txt iso-8859-1 http://ts10vm.bireme.br:8180/DeDup/services lilacs_Sas LILACS_Sas_Seven outdup1.txt outdup2.txt outnodup.txt utf-8
