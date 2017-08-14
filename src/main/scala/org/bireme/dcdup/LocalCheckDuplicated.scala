fe39eb68aca412a3717be9844158538c07815bbe
Heitor <heitor@bir927595.wdc.paho.org>
2017-08-14 11:56:36 -0300

    ngrams library is now ordering repetitive fields

---
+++ src/main/scala/org/bireme/dcdup/LocalCheckDuplicated.scala
@@ -0,0 +1,47 @@
+
+/*=========================================================================
+
+    Copyright © 2016 BIREME/PAHO/WHO
+
+    This file is part of DCDup.
+
+    DCDup is free software: you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 2.1 of
+    the License, or (at your option) any later version.
+
+    DCDup is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with DCDup. If not, see <http://www.gnu.org/licenses/>.
+
+=========================================================================*/
+
+package org.bireme.dcdup
+
+/** Checks a DeDup input piped file against itself to look for duplicated
+  * documents.
+  *
+  * author: Heitor Barbieri
+  * date: 20170803
+*/
+object LocalCheckDuplicated extends App {
+  private def usage(): Unit = {
+    System.err.println("usage: LocalCheckDuplicated " +
+      "\n\t<schemaFile> - DeDup schema file" +
+      "\n\t<schemaFileEncoding> - schema file character encoding" +
+      "\n\t<pipeFile> - DeDup piped input file" +
+      "\n\t<pipeFileEncoding> - pipe file character encoding" +
+      "\n\t<outDupFile> - duplicated records found in pipe file" +
+      "\n\t[<outDupEncoding>] - output file character encoding. Default is utf-8")
+    System.exit(1)
+  }
+
+  val outEncoding = if (args.size > 5) args(5) else "utf-8"
+
+  val dup = new WebDoubleCheckDuplicated()
+  dup.localCheck(args(0), args(1), args(2), args(3), args(4), outEncoding)
+}
--- src/main/scala/org/bireme/dcdup/WebDoubleCheckDuplicated.scala
+++ src/main/scala/org/bireme/dcdup/WebDoubleCheckDuplicated.scala
@@ -68,30 +68,6 @@ object WebDoubleCheckDuplicated extends App {
                   args(5), args(6), args(7), outEncoding)
 }
 
-/** Checks a DeDup input piped file against itself to look for duplicated
-  * documents.
-  *
-  * author: Heitor Barbieri
-  * date: 20170803
-*/
-object LocalCheckDuplicated extends App {
-  private def usage(): Unit = {
-    System.err.println("usage: LocalCheckDuplicated " +
-      "\n\t<schemaFile> - DeDup schema file" +
-      "\n\t<schemaFileEncoding> - schema file character encoding" +
-      "\n\t<pipeFile> - DeDup piped input file" +
-      "\n\t<pipeFileEncoding> - pipe file character encoding" +
-      "\n\t<outDupFile> - duplicated records found in pipe file" +
-      "\n\t[<outDupEncoding>] - output file character encoding. Default is utf-8")
-    System.exit(1)
-  }
-
-  val outEncoding = if (args.size > 5) args(5) else "utf-8"
-
-  val dup = new WebDoubleCheckDuplicated()
-  dup.localCheck(args(0), args(1), args(2), args(3), args(4), outEncoding)
-}
-
 /** Class to check a DeDup input piped file against itself to look for duplicated
   * documents and then against a Dedup index via DeDup webservice
   * to also look for duplicated docs.
