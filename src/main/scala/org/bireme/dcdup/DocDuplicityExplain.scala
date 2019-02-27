bc7d2c3a84d904281e3b196d214f70b1e43030a1
heitorbarbieri <superheitor@gmail.com>
2019-02-27 16:26:35 -0300

    version 2.0.5 added tools DocDuplicityExplain and SimilarFieldDocs

---
+++ src/main/scala/org/bireme/dcdup/DocDuplicityExplain.scala
@@ -0,0 +1,272 @@
+/*=========================================================================
+
+    DCDup © Pan American Health Organization, 2017.
+    See License at: https://github.com/bireme/DCDup/blob/master/LICENSE.txt
+
+  ==========================================================================*/
+
+package org.bireme.dcdup
+
+import br.bireme.ngrams._
+import org.apache.commons.text.StringEscapeUtils
+import org.apache.lucene.document.Document
+import org.apache.lucene.index.Term
+import org.apache.lucene.search.{IndexSearcher, TermQuery}
+import org.apache.lucene.search.spell.NGramDistance
+
+import scala.collection.mutable
+import collection.JavaConverters._
+
+/**
+* Object used to explain if a given input piped string representing a document is duplicated or not with another one
+  * stored in a Lucene index (created from NGrams library) and represented by a document id.
+  *
+  * Author: Heitor Barbieri
+  * Date: 20190227
+  */
+object DocDuplicityExplain extends App {
+  private def usage(): Unit = {
+    System.err.println("usage: DocDuplicityExplain")
+    System.err.println("\t\t-index=<indexPath> - Lucene index name/path")
+    System.err.println("\t\t-conf=<confFile> - xml configuration/schema file")
+    System.err.println("\t\t-id=<indexDocId> - id of the indexed document to compare")
+    System.err.println("\t\t-doc=<pipeInputDocument> - input piped document to compare")
+    System.err.println("\t\t[-encod=<confFileEncoding>] - xml configuration/schema file encoding")
+    System.exit(1)
+  }
+
+  if (args.length < 4) usage()
+
+  val parameters = args.foldLeft[Map[String, String]](Map()) {
+    case (map, par) =>
+      val split = par.split(" *= *", 2)
+      if (split.length == 2) map + ((split(0).substring(1), split(1)))
+      else {
+        usage()
+        map
+      }
+  }
+  val index = parameters("index")
+  val conf = parameters("conf")
+  val id = parameters("id")
+  val doc = parameters("doc")
+  val encod = parameters.getOrElse("encod", "utf-8")
+  val expl = explain(index, conf, encod, id, doc)
+
+  println(expl)
+
+  /**
+  * Explain if two documents are duplicated or not and why
+    * @param index lucene index path having one document
+    * @param conf NGrams/DeDup schema file
+    * @param encod NGrams/DeDup schema file encoding
+    * @param id identifier of the indexed document used to compare for duplicity
+    * @param doc input piped document used to compare for duplicity
+    * @return the report saying if the documents are duplicated or not and why
+    */
+  def explain(index: String,
+              conf: String,
+              encod: String,
+              id: String,
+              doc: String): String = {
+    val ngIndex: NGIndex = new NGIndex("index", index, true)
+    val schema: NGSchema = new NGSchema("schema", conf, encod)
+    val searcher: IndexSearcher = ngIndex.getIndexSearcher
+
+    parseInputDoc(doc, schema.getParameters) match {
+      case Some(doc2) => getDocument(id, searcher) match {
+        case Some(document) => checkAllFields(ngIndex, schema, doc2, document)
+        case None => s"Document id=$id not found."
+      }
+      case None => s"Parsing document error => $doc"
+    }
+  }
+
+  /**
+  *  Check each field of the input and indexed document, to see if they are similar of not
+    * @param ngIndex object representing the lucene index in the NGrams library
+    * @param schema object representing the document schema in the NGrams library
+    * @param doc input piped document used to check its fields
+    * @param doc2 indexed lucene document used to check its fields
+    * @return the report saying if the documents are duplicated or not and why
+    */
+  private def checkAllFields(ngIndex: NGIndex,
+                             schema: NGSchema,
+                             doc: Array[String],
+                             doc2: Document): String = {
+    val analyzer: NGAnalyzer = ngIndex.getAnalyzer.asInstanceOf[NGAnalyzer]
+    val ngDistance = new NGramDistance(analyzer.getNgramSize)
+    val params: Parameters = schema.getParameters
+    val fields: Map[Int, Field] = params.getSearchFields.asScala.map(kv => (kv._1.toInt, kv._2)).toMap
+    val fields2: java.util.Map[String, Field] = params.getNameFields
+
+
+    val results: Seq[(Int,Int)] = fields.foldLeft(Seq[(Int,Int)]()) {
+      case (seq, fld: (Int, Field)) =>
+        val check: Int = NGrams.checkField(ngDistance, fld._2, doc, fields2, doc2)
+        seq :+ ((fld._1, check))
+    }
+    val idxFldPos: Int = params.getIndexedPos
+    val similarity: Float = getSimilarity(doc, idxFldPos, doc2, fields(idxFldPos).name, ngDistance)
+
+    createReport(params, similarity, results, fields, doc, doc2)
+  }
+
+  /**
+  * Get the string similarity between the input text and the indexed field
+    * @param doc1 the input document
+    * @param idxFldPos the position of the input text to be compared
+    * @param doc2 the indexed document
+    * @param idxName the name of the indexed field
+    * @param ngDistance the object that will compare the distance
+    * @return the distance (similarity) of the two document strings
+    */
+  private def getSimilarity(doc1: Array[String],
+                            idxFldPos: Int,
+                            doc2: Document,
+                            idxName: String,
+                            ngDistance: NGramDistance): Float = {
+    val content1: String = br.bireme.ngrams.Tools.limitSize(
+      br.bireme.ngrams.Tools.normalize(doc1(idxFldPos), NGrams.OCC_SEPARATOR),
+      NGrams.MAX_NG_TEXT_SIZE).trim
+    val content2: String = {
+      val contents = doc2.getValues(idxName)
+      if (contents.isEmpty) "" else contents.head
+    }
+    ngDistance.getDistance(content1, content2)
+  }
+
+  /**
+  * Create an output report explaining why the two documents are duplicated or not
+    * @param parameters the representation of the index schema file
+    * @param similarity number indication how much two document are similar
+    * @param results the result of comparing the two document fields
+    * @param fields the (position, field) composition of the documents
+    * @param doc1 input piped document
+    * @param doc2 indexed document
+    * @return report explaining if the document are duplicated or not and why
+    */
+  private def createReport(parameters: Parameters,
+                           similarity: Float,
+                           results: Seq[(Int,Int)],
+                           fields: Map[Int, Field],
+                           doc1: Array[String],
+                           doc2: Document): String = {
+    val builder: StringBuilder =  StringBuilder.newBuilder
+
+    getDocs(doc1, doc2, fields, builder)
+
+    val (matchedFields: Int, maxScore: Boolean) = results.foldLeft(0, false) {
+      case ((tot,bool), (pos, result)) =>
+        result match {
+          case -1 =>
+            builder.append(s"\nField[$pos,'${fields(pos).name}']: does NOT match")
+            (tot, bool)
+          case -2 =>
+            builder.append(s"\nField[$pos,'${fields(pos).name}']: requires maximum similarity score")
+            (tot, true)
+          case 0 =>
+            builder.append(s"\nField[$pos,'${fields(pos).name}']: is ignored")
+            (tot, bool)
+          case _ =>
+            builder.append(s"\nField[$pos,'${fields(pos).name}']: matches")
+            (tot + 1, bool)
+        }
+    }
+    builder.append(s"\n\nSimilarity = $similarity")
+    builder.append(s"\nRequired_max_similarity_score = $maxScore\nTotal_fields = ${results.size}\nMatched_fields = $matchedFields")
+
+    getMinFields(parameters, similarity) match {
+      case Some(minFields) =>
+        builder.append(s"\nRequired_fields_match = $minFields")
+        if (maxScore) {
+          if (similarity == 1.0) {
+            if (matchedFields >= minFields) builder.append("\n\nDocuments are duplicated.")
+            else builder.append("\n\nDocuments are NOT duplicated. Matched fields[matchedFields] < $minFields.")
+          } else builder.append("\n\nDocuments are NOT duplicated. Similarity[$similarity] < 1.0")
+        } else {
+          if (matchedFields >= minFields) builder.append("\n\nDocuments are duplicated.")
+          else builder.append(s"\n\nDocuments are NOT duplicated. Matched fields[matchedFields] < $minFields.")
+        }
+      case None => builder.append(s"\n\nDocuments are NOT duplicated. Similarity[$similarity] < ${parameters.getMinSimilarity}.")
+    }
+    builder.toString()
+  }
+
+  /**
+  *
+    * @param doc1 input piped document
+    * @param doc2 indexed similar document
+    * @param fields name of the fields of the input and indexed document
+    * @param builder auxiliary string to append the both document fields
+    * @return a String with the input text fields and the similar document fields
+    */
+  private def getDocs(doc1: Array[String],
+                      doc2: Document,
+                      fields: Map[Int, Field],
+                      builder: StringBuilder): StringBuilder = {
+    builder.append("\nInput document:")
+    fields foreach {
+      case (pos, fld) => builder.append(s"\n${fld.name} => ${doc1(pos)}")
+    }
+
+    builder.append("\n\nIndexed document:")
+    fields foreach {
+      case (_, fld) =>
+        val notNormalized: String = fld.name + "~notnormalized"
+        builder.append(s"\n${fld.name} => ${doc2.getValues(notNormalized).head}")
+    }
+    builder.append("\n")
+  }
+
+  /**
+  * Giving a schema file, looks for the minimum number of matching fields for a given similarity
+    * @param parameters object representing the schema file
+    * @param similarity the distance from two documents
+    * @return the minimum number of matching fields of two documents required to be considered duplicated
+    */
+  private def getMinFields(parameters: Parameters,
+                           similarity: Float): Option[Int] = {
+    val scores: mutable.Seq[Score] = {
+      val buffer = mutable.Buffer.empty[Score]
+      val iterator = parameters.getScores.iterator()
+
+      while (iterator.hasNext) buffer += iterator.next()
+      buffer
+    }
+    scores.find(score => score.getMinValue <= similarity).map(_.getMinFields)
+  }
+
+  /**
+  * Parse a piped document string and convert it into a array
+    * @param doc input document
+    * @param params schema
+    * @return an array with document fields where the position is the same of the piped string
+    */
+  private def parseInputDoc(doc: String,
+                            params: Parameters): Option[Array[String]] = {
+    require(doc != null)
+
+    val split: Array[String] = StringEscapeUtils.unescapeHtml4(doc).replace(':', ' ').trim
+                                                   .split(" *\\| *", Integer.MAX_VALUE)
+    val fields: Map[Int, Field] = params.getSearchFields.asScala.map(kv =>  (kv._1.toInt, kv._2)).toMap
+
+    if (split.length == fields.size) Some(split)
+    else None
+  }
+
+  /**
+  * Retrieve a document whose field 'id' is docId
+    * @param docId document string identification
+    * @param searcher IndexSearcher Lucene object
+    * @return the Lucene document whose 'id' field is docId
+    */
+  private def getDocument(docId: String,
+                          searcher: IndexSearcher): Option[Document] = {
+    val query = new TermQuery(new Term("id", docId))
+
+    val topDocs = searcher.search(query, 1)
+    if (topDocs.totalHits == 0) None
+    else Some(searcher.doc(topDocs.scoreDocs.head.doc))
+  }
+}
---
+++ src/main/scala/org/bireme/dcdup/SimilarFieldDocs.scala
@@ -0,0 +1,237 @@
+/*=========================================================================
+
+    DCDup © Pan American Health Organization, 2017.
+    See License at: https://github.com/bireme/DCDup/blob/master/LICENSE.txt
+
+  ==========================================================================*/
+
+package org.bireme.dcdup
+
+import java.io.{BufferedWriter, File}
+import java.nio.charset.Charset
+import java.nio.file.Files
+
+import br.bireme.ngrams.{NGAnalyzer, NGrams}
+import org.apache.lucene.document.Document
+import org.apache.lucene.index.{DirectoryReader, MultiFields}
+import org.apache.lucene.search.spell.NGramDistance
+import org.apache.lucene.store.MMapDirectory
+import org.apache.lucene.util.Bits
+
+import scala.collection.immutable.TreeMap
+
+/**
+* Look for documents whose field is similar to the input text
+  *
+  * Author: Heitor Barbieri
+  * Date: 20190227
+  */
+object SimilarFieldDocs extends App {
+  private def usage(): Unit = {
+    System.err.println("usage: SimilarFieldDocs")
+    System.err.println("\t\t-fldText=<text> - input text used to find similar documents")
+    System.err.println("\t\t-index=<path> - path to the Lucene index")
+    System.err.println("\t\t-fieldName=<name> - document field used to compare with the input text")
+    System.err.println("\t\t-outFile=<file> - the output file where the similar documents will be placed")
+    System.err.println("\t\t[-outSize=<max>] - the maximum number of similar documents that will be outputed")
+    System.err.println("\t\t[--notNormalize] - do not normalize the input text")
+    System.exit(1)
+  }
+
+  if (args.length < 4) usage()
+
+  val parameters = args.foldLeft[Map[String, String]](Map()) {
+    case (map, par) =>
+      val split = par.split(" *= *", 2)
+      if (split.length == 1) map + ((split(0).substring(2), ""))
+      else if (split.length == 2) map + ((split(0).substring(1), split(1)))
+      else {
+        usage()
+        map
+      }
+  }
+
+  val fldText: String = parameters("fldText")
+  val index: String = parameters("index")
+  val fieldName: String = parameters("fieldName")
+  val outFile: String = parameters("outFile")
+  val outSize: Int = parameters.getOrElse("outSize", "10").toInt
+  val notNormalize: Boolean = parameters.getOrElse("notNormalize", "false").toBoolean
+
+  findSimilars(fldText, index, fieldName, outFile, notNormalize)
+
+  /**
+  * Find documents whose field is similiar to the input text
+    * @param fldText input text used to find similar documents
+    * @param index path to the Lucene index
+    * @param fldName document field used to compare with the input text
+    * @param outFile the output file where the similar documents will be placed
+    * @param notNormalize true if it is to not normalize the input text, false to normalize the input text
+    */
+  def findSimilars(fldText: String,
+                   index: String,
+                   fldName: String,
+                   outFile: String,
+                   notNormalize: Boolean): Unit = {
+    val inText: String = if (notNormalize) fldText.trim
+      else br.bireme.ngrams.Tools.limitSize(
+      br.bireme.ngrams.Tools.normalize(fldText, NGrams.OCC_SEPARATOR), NGrams.MAX_NG_TEXT_SIZE).trim
+    val ngDistance = new NGramDistance(new NGAnalyzer().getNgramSize)
+    val iterator: DocumentIterator = new DocumentIterator(index)
+    val writer: BufferedWriter = Files.newBufferedWriter(
+      new File(outFile).toPath, Charset.forName("utf-8"))
+    val reader: DirectoryReader = DirectoryReader.open(
+      new MMapDirectory(new File(index).toPath))
+
+    writer.write(fldText + "\n\n")
+
+    val sim : Map[Float, Set[Int]] =
+      findSimilars(inText, iterator, fldName, ngDistance, new TreeMap[Float,Set[Int]], 0, 0)
+
+    TreeMap[Float, Set[Int]]()(Ordering[Float].reverse) ++ sim foreach {
+      case (sim: Float, set: Set[Int]) => set foreach {
+        id: Int =>
+          val doc = reader.document(id)
+          val field: String = {
+            val aux: String = doc.get(fldName + "~notnormalized")
+            if (aux == null) "" else aux
+          }
+          val did: String = {
+            val aux: String = doc.get("id")
+            if (aux == null) "" else aux
+          }
+          if (!field.isEmpty && !did.isEmpty) writer.write(s"$sim|$did|$field\n")
+      }
+    }
+
+    reader.close()
+    iterator.close()
+    writer.close()
+  }
+
+  /**
+  * Find documents whose field is similiar to the input text
+    * @param inText input text used to find similar documents
+    * @param iterator (id, document) iterator
+    * @param fldName name of the field used to compare similarity with the input text
+    * @param ngDistance object used to calculate the distance/similarity between texts
+    * @param results auxiliary TreeMap
+    * @param stored number of already stored document ids in the TreeMap
+    * @param current order number of the current document to look for similiar ones
+    * @return a TreeMap of (similariy,doc Id) tuples found of the similar documents
+    */
+  private def findSimilars(inText: String,
+                           iterator: DocumentIterator,
+                           fldName: String,
+                           ngDistance: NGramDistance,
+                           results: TreeMap[Float,Set[Int]],
+                           stored: Int,
+                           current: Int): TreeMap[Float,Set[Int]] = {
+    if (current % 100000 == 0) println(s"+++$current")
+
+    if (iterator.hasNext) {
+      val (id: Int, doc: Document) = iterator.next()
+//println(s"id=$id")
+      val field: String = {
+        val aux: String = doc.get(fldName)
+        if (aux == null) "" else aux
+      }
+      val distance: Float = ngDistance.getDistance(inText, field)
+      val (results2, added) = results.lastOption match {
+        case Some(kv) =>
+          if (stored < outSize) (addElement(id, distance, results), true)
+          else {
+            if (distance > kv._1) {
+              val auxSet: Set[Int] = kv._2.tail
+              val resultLess : TreeMap[Float, Set[Int]] = if (auxSet.isEmpty) results - kv._1
+                                                      else results + (kv._1 -> kv._2.tail)
+              (addElement(id, distance, resultLess), false)
+            } else (results, false)
+          }
+        case None => (addElement(id, distance, results), true)
+      }
+      findSimilars(inText, iterator, fldName, ngDistance, results2, if (added) stored + 1 else stored, current + 1)
+    } else results
+  }
+
+  /**
+  * Add an (similarity,doc id) into the TreeSet
+    * @param id Lucene internal document identification
+    * @param distance similarity of two documents
+    * @param results auxiliary TreeMap
+    * @return the aux TreeMap with the element (similarity, doc id) inserted
+    */
+  private def addElement(id: Int,
+                         distance: Float,
+                         results: TreeMap[Float,Set[Int]]): TreeMap[Float,Set[Int]] = {
+    results.get(distance) match {
+      case Some(ids) => results + (distance -> (ids + id))
+      case None => results + (distance -> Set(id))
+    }
+  }
+}
+
+/**
+* Class that iterates over the documents of a Lucene index
+  * @param indexPath - Lucene index path
+  */
+class DocumentIterator(indexPath: String) extends Iterator[(Int, Document)] {
+  val reader: DirectoryReader = DirectoryReader.open(
+    new MMapDirectory(new File(indexPath).toPath))
+  val liveDocs: Bits = MultiFields.getLiveDocs(reader)
+  val max: Int = reader.maxDoc()
+  var cur: Int = 0
+  var elem: Option[(Int, Document)] = getNext
+
+  /**
+  * Internal hasNext of the Iterator interface
+    * @return true if has a next object of false otherwise
+    */
+  private def hasNext0: Boolean = {
+    val has : Boolean = (liveDocs != null) && (cur < max)
+    if (!has) close()
+    has
+  }
+
+  /**
+  *
+    * @return the next document from the index
+    */
+  private def getNext: Option[(Int,Document)] = {
+    if (hasNext0) {
+      if (liveDocs.get(cur)) {
+        cur += 1
+        Some((cur - 1, reader.document(cur - 1)))
+      } else {
+        cur += 1
+        getNext
+      }
+    } else None
+  }
+
+  /**
+  * See Iterator documentation
+    * @return true if has a next object of false otherwise
+    */
+  override def hasNext: Boolean = elem.isDefined
+
+  /**
+  * See Iterator documentation
+    * @return return the next tuple (internal lucene doc id, document object)
+    */
+  override def next(): (Int,Document) = {
+    if (hasNext) {
+      val ret: (Int, Document) = elem.get
+      elem = getNext
+      ret
+    }
+    else throw new NoSuchElementException()
+  }
+
+  /**
+  * Close open resource
+    */
+  def close(): Unit = {
+    reader.close()
+  }
+}
